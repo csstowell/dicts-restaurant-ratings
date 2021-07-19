a0229ad26c37052c1997a8b51333f6a0a2ae26fc
csstowell <charlottestowell@berkeley.edu>
2021-07-19 23:29:26 +0000

    wrote code to return a list of restaurants and ratings

--- ratings.py
+++ ratings.py
@@ -1,4 +1,31 @@
-"""Restaurant rating lister."""
+"""Restaurant rating lister:
+
+1. Reads the ratings in from the file
+
+2. Stores them in a dictionary
+
+3. And finally, spits out the ratings in alphabetical order by restaurant
+
+"""
 
 
-# put your code here
+# open scores.txt file
+file = open('scores.txt')
+
+def restaurant_rating(file):
+
+    # read ratings in from the file
+    # store them in a dictionary
+    restaurant_ratings = {}
+
+
+    for restaurant in file:
+        
+        restaurant_ratings[restaurant] = restaurant_ratings.get(restaurant, 0) + 1
+       
+    return restaurant_ratings
+        
+
+        
+
+    # outputs restaurant ratings in alphabetical order
