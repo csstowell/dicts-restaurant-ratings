19aee0e2247643fb5afcebae99b5a16290879f15
csstowell <charlottestowell@berkeley.edu>
2021-08-16 19:47:20 +0000

    Final Commit

--- ratings.py
+++ ratings.py
@@ -1,31 +1,42 @@
-"""Restaurant rating lister:
+"""Restaurant rating lister.
 
 1. Reads the ratings in from the file
 
 2. Stores them in a dictionary
 
 3. And finally, spits out the ratings in alphabetical order by restaurant
-
 """
 
-
-# open scores.txt file
-file = open('scores.txt')
-
-def restaurant_rating(file):
-
-    # read ratings in from the file
-    # store them in a dictionary
+def process_file(filepath):
+    
+    #open the file
+    file = open(filepath)
+    # empty dictionary 
     restaurant_ratings = {}
 
+    #loop each line of the file
+    for line in file:
+        #removes any trailing whitespace
+        line = line.rstrip()
+        words = line.split(":") # words = ["restaurant name", "5"]
+        restaurant_ratings[words[0]] = words[1]
 
-    for restaurant in file:
-        
-        restaurant_ratings[restaurant] = restaurant_ratings.get(restaurant, 0) + 1
-       
     return restaurant_ratings
-        
 
-        
+    
+def print_ordered_restaurant_ratings(restaurant_ratings):  
+    
+    for restaurant in sorted(restaurant_ratings):
+        print(f"{restaurant} is rated at {restaurant_ratings[restaurant]}")  
 
-    # outputs restaurant ratings in alphabetical order
+
+def user_add_restaurant(restaurant_ratings):
+    
+    rest_name = input("restaurant name: ").title()
+    rest_score = input("score: ")
+
+    restaurant_ratings[rest_name] = rest_score
+
+ratings = process_file("scores.txt")  
+user_add_restaurant(ratings) 
+print_ordered_restaurant_ratings(ratings)
